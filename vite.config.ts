39109f1d63629f78a272fffe2112d6753078df67
Rudi Larno <rudi.larno@gmail.com>
2025-12-06 20:37:27 +0100

    Update vite config to run in github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/primo-model-showcase-47310e26/' : '/',
   server: {
     host: "::",
     port: 8080,
